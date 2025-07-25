6300dd621695fbdaeff436d82815f10d747e39cb
Marco Parra <marcoparra311@gmail.com>
2025-07-25 15:13:31 -0400

    feat(gifs): Create environment file structure

--- gifs-app/src/app/gifs/components/gifs-side-menu/gifs-side-menu-header/gifs-side-menu-header.component.ts
+++ gifs-app/src/app/gifs/components/gifs-side-menu/gifs-side-menu-header/gifs-side-menu-header.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { environment } from '../../../../../environments/environment';
+
 
 @Component({
   selector: 'gifs-side-menu-header',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './gifs-side-menu-header.component.html',
 })
-export default class GifsSideMenuHeaderComponent { }
+export default class GifsSideMenuHeaderComponent {
+  public envs = signal(environment);
+}
