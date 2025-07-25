c1ebff9d2bea78bb16f0175deafa77ca73572df6
Marco Parra <marcoparra311@gmail.com>
2025-07-25 12:45:40 -0400

    feat(gifs): - Create routes and route children - Creeate dashboard, trending and search components - Create dashboard structure on dashboard component - Create router outlet for dashboard children routes

---
+++ gifs-app/src/app/app.routes.ts
@@ -0,0 +1,30 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./gifs/pages/dashboard/dashboard.component'),
+
+    children: [
+      {
+        path: 'trending',
+        loadComponent: () => import('./gifs/pages/trending/trending.component'),
+      },
+      {
+        path: 'search',
+        loadComponent: () => import('./gifs/pages/search/search.component'),
+      },
+      {
+        path: '**',
+        redirectTo: 'trending',
+        pathMatch: 'full',
+      },
+    ],
+  },
+
+  {
+    path: '**',
+    redirectTo: 'dashboard',
+    pathMatch: 'full',
+  },
+];
---
+++ gifs-app/src/app/gifs/pages/dashboard/dashboard.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-dashboard',
+  imports: [RouterOutlet],
+  templateUrl: './dashboard.component.html',
+})
+export default class DashboardComponent { }
