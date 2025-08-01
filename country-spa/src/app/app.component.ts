671b561f19a7287575bc48b2a832739f76767426
Marco Parra <marcoparra311@gmail.com>
2025-08-01 12:50:05 -0400

    feat(country-spa): Move footer component to app main component

--- country-spa/src/app/app.component.ts
+++ country-spa/src/app/app.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+
+import { FooterComponent } from './shared/components/footer/footer.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
--- country-spa/src/app/shared/pages/home/home.component.ts
+++ country-spa/src/app/shared/pages/home/home.component.ts
@@ -2,11 +2,10 @@ import { Component } from '@angular/core';
 
 import { HeaderSectionComponent } from '../../components/header-section/header-section.component';
 import { NavSectionComponent } from '../../components/nav-section/nav-section.component';
-import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'home',
-  imports: [FooterComponent, HeaderSectionComponent, NavSectionComponent],
+  imports: [HeaderSectionComponent, NavSectionComponent],
   templateUrl: './home.component.html',
 })
 export class HomeComponent { }
