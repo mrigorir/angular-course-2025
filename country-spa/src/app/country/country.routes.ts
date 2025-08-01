19f95544bcd4042b8b59e18830bd4af81c380346
Marco Parra <marcoparra311@gmail.com>
2025-08-01 10:22:45 -0400

    feat(country-spa): Create layout component

--- country-spa/src/app/country/country.routes.ts
+++ country-spa/src/app/country/country.routes.ts
@@ -1,13 +1,21 @@
 import { Routes } from '@angular/router';
+import ByCapitalComponent from './pages/by-capital/by-capital.component';
 
 export const countryRoutes: Routes = [
   {
     path: '',
-    loadComponent: () => import('./pages/by-capital/by-capital.component'),
-  },
-  {
-    path: '**',
-    redirectTo: '',
+    loadComponent: () =>
+      import('./layouts/country-layout/country-layout.component'),
+    children: [
+      {
+        path: 'by-capital',
+        component: ByCapitalComponent,
+      },
+      {
+        path: '**',
+        redirectTo: 'by-capital',
+      },
+    ],
   },
 ];
 
---
+++ country-spa/src/app/country/layouts/country-layout/country-layout.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'country-layout',
+  imports: [RouterOutlet],
+  templateUrl: './country-layout.component.html',
+})
+export default class CountryLayoutComponent { }
