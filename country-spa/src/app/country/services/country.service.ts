3a731f99fa99f4c90824bb057d6e9a3f926be090
Marco Parra <marcoparra311@gmail.com>
2025-08-07 09:28:25 -0400

    feat(country-spa): Create results cache store using Map feature

--- country-spa/src/app/country/services/country.service.ts
+++ country-spa/src/app/country/services/country.service.ts
@@ -8,7 +8,7 @@ import { RestCountries } from '../interfaces/countries';
 
 import { CountryMapper } from '../mappers/country.mappers';
 
-import { catchError, map, Observable, throwError } from 'rxjs';
+import { catchError, map, Observable, of, tap, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -17,14 +17,20 @@ export class CountryService {
   private http = inject(HttpClient);
 
   private readonly base_url = environment.api_url;
+  private queryCacheCapital = new Map<string, Country[]>();
 
   searchByCapital(query: string): Observable<Country[]> {
     query = query.toLocaleLowerCase();
+
+    if (this.queryCacheCapital.has(query)) {
+      return of(this.queryCacheCapital.get(query) ?? []);
+    }
 
     return this.http
       .get<RestCountries[]>(`${this.base_url}/capital/${query}`)
       .pipe(
         map((resp) => CountryMapper.mapRestCountriesArrayToCountryArray(resp)),
+        tap((countries) => this.queryCacheCapital.set(query, countries)),
         catchError((error) => {
           console.error('Error: ', error);
           return throwError(() => new Error('Capital not found. ', error));
@@ -46,16 +52,17 @@ export class CountryService {
       );
   }
 
-   searchByCountryByAlphaCode(code: string) {
-
+  searchByCountryByAlphaCode(code: string) {
     return this.http
       .get<RestCountries[]>(`${this.base_url}/alpha/${code}`)
       .pipe(
         map((resp) => CountryMapper.mapRestCountriesArrayToCountryArray(resp)),
-        map(countries => countries.at(0)),
+        map((countries) => countries.at(0)),
         catchError((error) => {
           console.error('Error: ', error);
-          return throwError(() => new Error(`Country not found by code: ${code}`, error));
+          return throwError(
+            () => new Error(`Country not found by code: ${code}`, error)
+          );
         })
       );
   }
