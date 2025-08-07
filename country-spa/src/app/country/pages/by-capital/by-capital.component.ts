bfc864bb7a97e65124e0f917982f085dd0e46a43
Marco Parra <marcoparra311@gmail.com>
2025-08-07 12:09:07 -0400

    feat(country-spa): Repeat query params methods on country component

--- country-spa/src/app/country/pages/by-capital/by-capital.component.ts
+++ country-spa/src/app/country/pages/by-capital/by-capital.component.ts
@@ -8,7 +8,7 @@ import { firstValueFrom, of } from 'rxjs';
 
 import { CountrySearchInputComponent } from '../../components/country-search-input/country-search-input.component';
 import { CountryListComponent } from '../../components/country-list/country-list.component';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'by-capital',
@@ -18,6 +18,7 @@ import { ActivatedRoute } from '@angular/router';
 export default class ByCapitalComponent {
   private countryService = inject(CountryService);
   activatedRoute = inject(ActivatedRoute);
+  router = inject(Router);
 
   queryParam = this.activatedRoute.snapshot.queryParamMap.get('query') ?? '';
 
@@ -32,6 +33,10 @@ export default class ByCapitalComponent {
     request: () => ({ query: this.query() }),
     loader: ({ request }) => {
       if (!request.query) return of([]);
+
+      this.router.navigate(['/country/by-capital'], {queryParams: {
+        query: request.query,
+      }});
 
       return this.countryService.searchByCapital(request.query);
     },
--- country-spa/src/app/country/pages/by-country/by-country.component.ts
+++ country-spa/src/app/country/pages/by-country/by-country.component.ts
@@ -1,10 +1,17 @@
-import { Component, inject, resource, signal } from '@angular/core';
+import {
+  Component,
+  inject,
+  linkedSignal,
+  resource,
+  signal,
+} from '@angular/core';
 
 import { CountrySearchInputComponent } from '../../components/country-search-input/country-search-input.component';
 import { CountryListComponent } from '../../components/country-list/country-list.component';
 import { firstValueFrom, of } from 'rxjs';
 import { CountryService } from '../../services/country.service';
 import { rxResource } from '@angular/core/rxjs-interop';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'by-country',
@@ -13,8 +20,12 @@ import { rxResource } from '@angular/core/rxjs-interop';
 })
 export default class ByCountryComponent {
   private countryService = inject(CountryService);
+  activatedRoute = inject(ActivatedRoute);
+  router = inject(Router);
 
-  query = signal<string>('');
+  queryParam = this.activatedRoute.snapshot.queryParamMap.get('query') ?? '';
+
+  query = linkedSignal<string>(() => this.queryParam);
 
   onSearch(value: string) {
     this.query.set(value);
@@ -25,6 +36,12 @@ export default class ByCountryComponent {
     request: () => ({ query: this.query() }),
     loader: ({ request }) => {
       if (!request.query) return of([]);
+
+      this.router.navigate(['/country/by-country'], {
+        queryParams: {
+          query: request.query,
+        },
+      });
 
       return this.countryService.searchByCountry(request.query);
     },
