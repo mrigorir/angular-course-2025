2ed50b5d96ba6ba56a31b7c427de620b99733dba
Marco Parra <marcoparra311@gmail.com>
2025-08-11 11:15:14 -0400

    feat(pipes): Create number pipe structure (currency, percent, decimals)

--- pipes/src/app/pages/basic/basic.component.ts
+++ pipes/src/app/pages/basic/basic.component.ts
@@ -1,9 +1,14 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'basic',
-  imports: [],
+  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe],
   templateUrl: './basic.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class BasicComponent { }
+export default class BasicComponent {
+  nameLower = signal<string>('marco');
+  nameUpper = signal<string>('MARCO');
+  fullName = signal<string>('mArCo PArrA');
+}
--- pipes/src/app/pages/numbers/numbers.component.ts
+++ pipes/src/app/pages/numbers/numbers.component.ts
@@ -1,9 +1,13 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { CurrencyPipe, DecimalPipe, PercentPipe } from '@angular/common';
 
 @Component({
   selector: 'numbers',
-  imports: [],
+  imports: [DecimalPipe, CurrencyPipe, PercentPipe],
   templateUrl: './numbers.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class NumbersComponent { }
+export default class NumbersComponent {
+  totalSells = signal<number>(2_433_232.5567);
+  percent = signal<number>(0.4856);
+}
