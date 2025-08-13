ffbb983f0cc0f5aee448a7360f8b51432c2e62a4
Marco Parra <marcoparra311@gmail.com>
2025-08-13 13:22:58 -0400

    feat(forms): - Create error messages on HTML to bw shown - Create logic to manage form errors on touching a form field - Reset form when it turns to valid on saving data

--- reactive-forms/src/app/reactive/pages/basic/basic.component.ts
+++ reactive-forms/src/app/reactive/pages/basic/basic.component.ts
@@ -18,12 +18,49 @@ export default class BasicComponent {
   private fb = inject(FormBuilder);
 
   basicForm: FormGroup = this.fb.group({
-    price: ['', [Validators.required, Validators.minLength(3)]],
-    product: [0, [Validators.required, Validators.min(10)]],
+    product: [0, [Validators.required, Validators.minLength(10)]],
     inStorage: [0, [Validators.required, Validators.min(0)]],
+    price: ['', [Validators.required, Validators.min(3)]],
   });
 
   get basicFormValues() {
     return this.basicForm.controls;
+  }
+
+  isValidField(fieldName: string): boolean | null {
+    return (
+      !!this.basicFormValues[fieldName].errors &&
+      this.basicFormValues[fieldName].touched
+    );
+  }
+
+  getFieldError(fieldName: string): string | null {
+    if (!this.basicFormValues[fieldName]) return null;
+
+    const errors = this.basicFormValues[fieldName].errors ?? {};
+
+    for (const key of Object.keys(errors)) {
+      switch (key) {
+        case 'required':
+          return 'Este campo es requerido';
+
+        case 'minlength':
+          return `Mínimo de ${errors['minlength'].requiredLength} caracteres.`;
+
+        case 'min':
+          return `Valor mínimo de ${errors['min'].min} caracteres.`;
+      }
+    }
+
+    return null;
+  }
+
+  onSave() {
+    if(this.basicForm.invalid) {
+      this.basicForm.markAllAsTouched();
+      return;
+    }
+
+    this.basicForm.reset();
   }
 }
