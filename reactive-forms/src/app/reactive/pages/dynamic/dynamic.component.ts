af12b56e44ca699b99dc5a488e0505968f112325
Marco Parra <marcoparra311@gmail.com>
2025-08-14 12:42:10 -0400

    feat(forms): Create fileds in array

--- reactive-forms/src/app/reactive/pages/dynamic/dynamic.component.ts
+++ reactive-forms/src/app/reactive/pages/dynamic/dynamic.component.ts
@@ -1,11 +1,28 @@
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { JsonPipe } from '@angular/common';
-
-import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'dynamic',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './dynamic.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class DynamicComponent { }
+export default class DynamicComponent {
+  private fb = inject(FormBuilder);
+
+  dynamicForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    favoriteGames: this.fb.array(
+      [
+        ['Metal Gear', Validators.required],
+        ['Death Stranding', Validators.required],
+      ],
+      Validators.minLength(3)
+    ),
+  });
+
+  get favoriteGames() {
+    return this.dynamicForm.get('favoriteGames') as FormArray;
+  }
+}
