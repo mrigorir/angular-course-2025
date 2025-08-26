9402b47fa8377afb01d08d585e47f816e457e3cd
Marco Parra <marcoparra311@gmail.com>
2025-08-26 11:51:55 -0400

    feat(form): swtiches

--- reactive-forms/src/app/reactive/pages/switches/switches.component.ts
+++ reactive-forms/src/app/reactive/pages/switches/switches.component.ts
@@ -1,10 +1,35 @@
 import { JsonPipe } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+
+import { FormUtils } from '../../../utils/form-utils';
 
 @Component({
   selector: 'switches',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './switches.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class SwitchesComponent { }
+export default class SwitchesComponent {
+  private fb = inject(FormBuilder);
+
+  formUtils = FormUtils;
+
+  swtichesForm = this.fb.group({
+    gender: ['', Validators.required],
+    wantNotifications: [false],
+    termsAndConditions: [false, Validators.requiredTrue],
+  });
+
+  get swtichesFormValues() {
+    return this.swtichesForm.controls;
+  }
+
+  onSubmit() {
+    this.swtichesForm.markAllAsTouched();
+
+    if (this.swtichesForm.invalid) return;
+    
+    console.log(this.swtichesForm.valid);
+  }
+ }
