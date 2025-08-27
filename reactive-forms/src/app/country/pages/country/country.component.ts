590ee4e937b585ff1938177baaa3daf0425f76ca
Marco Parra <marcoparra311@gmail.com>
2025-08-27 10:17:06 -0400

    feat(forms): Populate countries selector using rxjs operators and effeec to clean susbcription

--- reactive-forms/src/app/country/pages/country/country.component.ts
+++ reactive-forms/src/app/country/pages/country/country.component.ts
@@ -1,8 +1,15 @@
 import { JsonPipe } from '@angular/common';
-import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  effect,
+  inject,
+  signal,
+} from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CountryService } from '../../services/country.service';
 import { Country } from '../../interfaces/country.interface';
+import { switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'country',
@@ -17,11 +24,34 @@ export default class CountryComponent {
 
   regions = signal(this.countyService.regions);
   countriesByRegion = signal<Country[]>([]);
-  borders = signal<Country[]>([]);
+  border = signal<Country[]>([]);
 
   countryForm = this.fb.group({
     region: ['', Validators.required],
     country: ['', Validators.required],
     border: ['', Validators.required],
   });
+
+  onFormChanged = effect((onCleanup) => {
+    const regionSubscription = this.onRegionChanged();
+
+    onCleanup(() => {
+      regionSubscription?.unsubscribe();
+    });
+  });
+
+  onRegionChanged() {
+    return this.countryForm
+      .get('region')
+      ?.valueChanges.pipe(
+        tap(() => this.countryForm.get('country')?.setValue('')),
+        tap(() => this.countryForm.get('border')?.setValue('')),
+        switchMap((region) =>
+          this.countyService.getCountriesByRegion(region ?? '')
+        )
+      )
+      .subscribe((countries) => {
+        this.countriesByRegion.set(countries);
+      });
+  }
 }
