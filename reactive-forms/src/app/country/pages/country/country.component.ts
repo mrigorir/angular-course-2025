1b4e86b0206df12780f2b154fc9b36360d0e3642
Marco Parra <marcoparra311@gmail.com>
2025-08-27 09:51:34 -0400

    feat(forms): Populate regions selector

--- reactive-forms/src/app/country/pages/country/country.component.ts
+++ reactive-forms/src/app/country/pages/country/country.component.ts
@@ -1,9 +1,27 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CountryService } from '../../services/country.service';
+import { Country } from '../../interfaces/country.interface';
 
 @Component({
   selector: 'country',
-  imports: [],
+  imports: [ReactiveFormsModule, JsonPipe],
   templateUrl: './country.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class CountryComponent { }
+export default class CountryComponent {
+  private fb = inject(FormBuilder);
+
+  countyService = inject(CountryService);
+
+  regions = signal(this.countyService.regions);
+  countriesByRegion = signal<Country[]>([]);
+  borders = signal<Country[]>([]);
+
+  countryForm = this.fb.group({
+    region: ['', Validators.required],
+    country: ['', Validators.required],
+    border: ['', Validators.required],
+  });
+}
