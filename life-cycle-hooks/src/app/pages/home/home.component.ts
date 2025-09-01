1cb0d584aa8628ecc0fe674351c9781cdd786a44
Marco Parra <marcoparra311@gmail.com>
2025-09-01 10:08:42 -0400

    feat(life-cycles): Create 7 life cicles hooks with personalized console.log

--- life-cycle-hooks/src/app/pages/home/home.component.ts
+++ life-cycle-hooks/src/app/pages/home/home.component.ts
@@ -1,5 +1,22 @@
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  DoCheck,
+  OnChanges,
+  SimpleChanges,
+  type OnInit,
+} from '@angular/core';
 
-import { ChangeDetectionStrategy, Component, type OnInit } from '@angular/core';
+const log = (color: string, ...messages: string[]) => {
+  console.log(
+    `${messages[0]} %c${messages.slice(1).join(', ')}`,
+    `color: ${color}`
+  );
+};
 
 @Component({
   selector: 'home',
@@ -7,8 +24,61 @@ import { ChangeDetectionStrategy, Component, type OnInit } from '@angular/core';
   templateUrl: './home.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent
+  implements
+    OnInit,
+    OnChanges,
+    DoCheck,
+    AfterContentChecked,
+    AfterContentInit,
+    AfterViewInit,
+    AfterViewChecked
+{
+  ngOnInit(): void {
+    log(
+      'red',
+      'ngOnInit',
+      '	Runs once after Angular has initialized all the components inputs.'
+    );
+  }
 
-  ngOnInit(): void { }
+  ngOnChanges(changes: SimpleChanges): void {
+    log('blue','ngOnChanges', 'Runs every time the components inputs have changed.');
+  }
 
+  ngDoCheck(): void {
+    log('green', 'ngDoCheck', 'Runs every time this component is checked for changes.');
+  }
+
+  ngAfterContentChecked(): void {
+    log(
+      'lightblue',
+      'ngAfterContentChecked',
+      'Runs every time this component content has been checked for changes.'
+    );
+  }
+
+  ngAfterContentInit(): void {
+    log(
+      '#bada55',
+      'ngAfterContentInit',
+      'Runs once after the components content has been initialized'
+    );
+  }
+
+  ngAfterViewChecked(): void {
+    log(
+      'cyan',
+      'ngAfterViewChecked',
+      'Runs every time the components view has been checked for changes.'
+    );
+  }
+
+  ngAfterViewInit(): void {
+    log(
+      'tomato',
+      'ngAfterViewInit',
+      'Runs once after the components view has been initialized.'
+    );
+  }
 }
