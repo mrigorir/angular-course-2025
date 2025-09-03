bd3dcc3b04fc1984dd4e297f87a0fa8c2f766695
Marco Parra <marcoparra311@gmail.com>
2025-09-03 10:46:45 -0400

    Create navbar logic with @for and toSignal to change pageTitle using observables

--- maps/src/app/shared/components/navbar/navbar/navbar.component.ts
+++ maps/src/app/shared/components/navbar/navbar/navbar.component.ts
@@ -1,15 +1,30 @@
-import { ChangeDetectionStrategy, Component, input, type OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { NavigationEnd, Router, RouterLink } from '@angular/router';
+
+import { toSignal } from '@angular/core/rxjs-interop';
+
+import { routes } from '../../../../app.routes';
+import { filter, map } from 'rxjs';
 
 @Component({
   selector: 'navbar',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './navbar.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent {
+  router = inject(Router);
 
+  routes = routes
+    .map(({ path, title }) => ({
+      path,
+      title: `${title ?? 'Mapas en Angular'}`,
+    }))
+    .filter((route) => route.path !== '**');
 
-
-  ngOnInit(): void { }
-
+  pageTitle = toSignal(this.router.events.pipe(
+    filter((event) => event instanceof NavigationEnd),
+    map((event) => event.url.slice(1))
+  ));
 }
