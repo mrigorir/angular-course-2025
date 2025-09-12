7056cb83eb8fd862ffc6eed33a4527d8aefa59fe
Marco Parra <marcoparra311@gmail.com>
2025-09-12 10:13:47 -0400

    feat(teloshop): Create houses component

---
+++ teloshop/src/app/pages/home/houses/houses.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'houses',
+  imports: [],
+  templateUrl: './houses.component.html',
+  styleUrl: './houses.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class HousesComponent { }
