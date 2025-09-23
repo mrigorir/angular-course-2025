f0270b8d001a11d7b2d0479a08ea563faadf29bc
Marco Parra <marcoparra311@gmail.com>
2025-09-23 12:14:06 -0400

    feat(teloshop): Populate products on product card component

--- teloshop/src/app/products/components/product-card/product-card.component.ts
+++ teloshop/src/app/products/components/product-card/product-card.component.ts
@@ -1,11 +1,14 @@
-import { Component, type OnInit } from '@angular/core';
+import { Product } from '@/products/interfaces/products.intertface';
+import { Component, input, type OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { SlicePipe } from '@angular/common';
 
 @Component({
   selector: 'product-card',
-  imports: [RouterLink],
+  imports: [RouterLink, SlicePipe],
   templateUrl: './product-card.component.html',
 })
 export class ProductCardComponent implements OnInit {
+  product = input.required<Product>();
   ngOnInit(): void {}
 }
