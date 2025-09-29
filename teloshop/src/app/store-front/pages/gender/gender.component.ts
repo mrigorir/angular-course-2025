be149cf279a3b062140bf0658d5b265727a91e4a
Marco Parra <marcoparra311@gmail.com>
2025-09-29 10:24:18 -0400

    feat(gender): Populate products depending on gender

--- teloshop/src/app/store-front/pages/gender/gender.component.ts
+++ teloshop/src/app/store-front/pages/gender/gender.component.ts
@@ -1,13 +1,27 @@
-import { ChangeDetectionStrategy, Component, type OnInit } from '@angular/core';
+import { ProductCardComponent } from '@/products/components/product-card/product-card.component';
+import { ProductsService } from '@/products/services/products.service';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { rxResource, toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'gender',
-  imports: [],
+  imports: [ProductCardComponent],
   templateUrl: './gender.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class GenderComponent implements OnInit {
+export default class GenderComponent {
+  route = inject(ActivatedRoute);
 
-  ngOnInit(): void { }
+  gender = toSignal(this.route.params.pipe(map(({ gender }) => gender)));
 
+  productsService = inject(ProductsService);
+
+  productsResource = rxResource({
+    params: () => ({ gender: this.gender() }),
+    stream: () => {
+      return this.productsService.getProducts({ gender: this.gender()});
+    },
+  });
 }
